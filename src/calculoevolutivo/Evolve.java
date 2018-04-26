244f0fc464f9bcf51810214f39cc66fac7da869e
Victor Tassinari <victorcelsoct@hotmail.com>
2018-04-26 17:03:19 -0300

    novas alterações

--- src/calculoevolutivo/Evolve.java
+++ src/calculoevolutivo/Evolve.java
@@ -13,44 +13,60 @@ import java.util.List;
  * @author v.tassinari
  */
 public class Evolve {
+
     private static Evolve instance;
     private double parentFitness;
     private double fitness;
     private double cellsPop;
 
     public static Evolve getInstance() {
-        if(instance == null){
-            instance  = new Evolve();
+        if (instance == null) {
+            instance = new Evolve();
         }
         return instance;
     }
-    
+
     /**
      *
      * @param quantity
      * @return a probabilidade de uma celula{@link Cell} se multiplicar
      * @see Cell
      */
-    public double cellWeight(double quantity){
-        return quantity/cellsPop;
+    public double cellWeight(double quantity) {
+        return quantity / cellsPop;
     }
-    
+
     /**
      *
      * @param x
      * @return a avaliação de cada individuo {@link Cell}
      */
-    public double evaluate(int x){
-        return -(Math.pow(3*x, 4)-Math.pow(20*x, 3)+Math.pow(42*x, 2)-(36*x)+2);
+    public double evaluate(int x) {
+        return -(Math.pow(3 * x, 4) - Math.pow(20 * x, 3) + Math.pow(42 * x, 2) - (36 * x) + 2);
     }
-    
+
     /**
      *
      * @return a população gerada
      */
-    public List<Cell> generatePop(){
+    public List<Cell> generatePop(List<Cell> population) {
         List<Cell> pop = new ArrayList<>();
-        
+        if (population.isEmpty()) {
+            cellsPop = 0;
+            for (int x = 0; x <= 20; x++) {
+                Cell unity = new Cell();
+                unity.setValue(x);
+                unity.setQuantity(evaluate(x - 10));
+                cellsPop += unity.getQuantity();
+                population.add(unity);
+            }
+            population.forEach(cell->{
+                cell.setWeight(cellWeight(cell.getQuantity()));
+            });
+            return population;
+        }else{
+            
+        }
         return pop;
     }
 
@@ -88,7 +104,8 @@ public class Evolve {
 
     /**
      *
-     * @return o total de populações de individuos que podem ser gerados para uma nova população
+     * @return o total de populações de individuos que podem ser gerados para
+     * uma nova população
      */
     public double getCellsPop() {
         return cellsPop;
